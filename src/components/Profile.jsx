d3b9f19a8d37b849fb69797e4120e8381ad54c76
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2021-12-09 17:34:33 +0900

    Authentication : Profile Data

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,6 +2,14 @@ import React, { Component } from 'react'
 
 class Profile extends Component {
   render() {
+    let name
+    let email
+
+    if (this.props.user) {
+      name = this.props.user.name
+      email = this.props.user.email
+    }
+
     return (
       <div>
         <br />
@@ -10,8 +18,8 @@ class Profile extends Component {
           <div className="jumbotron col-lg-4 offset-lg-4">
             <h3 className="text-center">User Profile</h3>
             <ul className="list-group">
-              <li className="list-group-item">Name : name</li>
-              <li className="list-group-item">Email : Eamil</li>
+              <li className="list-group-item">Name : {name}</li>
+              <li className="list-group-item">Email : {email}</li>
             </ul>
           </div>
         </div>
